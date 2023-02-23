ba52e94db4db149883ca6283d159ebd87f167146
fujiyamaorange <kaito.bakuman@gmail.com>
2023-02-23 21:47:39 +0900

    STEP1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use convert_case::{Case, Casing};
 use swc_core::ecma::{
-    ast::Program,
+    ast::{Pat, Program, VarDecl},
     transforms::testing::test,
     visit::{as_folder, FoldWith, VisitMut},
 };
@@ -7,7 +8,26 @@ use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata
 
 pub struct TransformVisitor;
 
+/**
+ * Convert to snake-case
+ */
+fn convert_to_snake_case(s: String) -> String {
+    return s.to_case(Case::Snake).to_lowercase();
+}
+
 impl VisitMut for TransformVisitor {
+    fn visit_mut_var_decl(&mut self, n: &mut VarDecl) {
+        let decls = &mut n.decls;
+
+        for decl in decls.iter_mut() {
+            let name = &mut decl.name;
+
+            if let Pat::Ident(binding_ident) = name {
+                let id = &mut binding_ident.id;
+                id.sym = convert_to_snake_case(id.sym.to_string()).into();
+            }
+        }
+    }
     // Implement necessary visit_mut_* methods for actual custom transform.
     // A comprehensive list of possible visitor methods can be found here:
     // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
@@ -21,9 +41,13 @@ pub fn process_transform(program: Program, _metadata: TransformPluginProgramMeta
 test!(
     Default::default(),
     |_| as_folder(TransformVisitor),
-    boo,
+    test1,
     // Input codes
-    r#"console.log("transform");"#,
+    r#"
+    const yourName = "KIMINONAWA";
+    "#,
     // Output codes after transformed with plugin
-    r#"console.log("transform");"#
+    r#"
+    const your_name = "KIMINONAWA";
+    "#
 );
